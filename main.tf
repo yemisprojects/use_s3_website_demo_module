c9a728abb171aadc3e34c434f2055441033d80e3
Yemi Odunade <oluwayemisi.odunade@cloudreach.com>
2022-11-22 22:03:28 -0500

    update main file argument

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "my_static_website" {
   source = "github.com/yemisprojects/s3_website_module_demo?ref=v0.0.1"
 
   bucket_name          = var.bucket_name
-  create_random_suffix = false
+  create_random_suffix = true
   environment          = var.environment
 
   tags = var.tags
